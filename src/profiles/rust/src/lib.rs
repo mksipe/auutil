f18b51d47946330f1070355d7271eff01f929c12
Mason Sipe <masonsipe0513@gmail.com>
2022-06-26 02:06:30 -0500

    Added additional entry to rust.lib

--- src/profiles/rust/src/lib.rs
+++ src/profiles/rust/src/lib.rs
@@ -1,15 +1,21 @@
 use std::process::Command;
 const ERRMSG:&str = "Something went wrong";
 
-fn update() -> i32 {
+fn updaterustup() -> i32 {
 
     let cmd = Command::new("rustup").arg("update").status().expect(ERRMSG);
     return cmd.code().unwrap();
 }
 
+fn updatecargo() -> i32 {
+
+    let cmd = Command::new("cargo").arg("update").status().expect(ERRMSG);
+    return cmd.code().unwrap();
+}
+
 pub fn checkinstallation(input: bool) -> Vec<i32> {
     if input == true {
-        return vec![update()];
+        return vec![updaterustup(), updatecargo()];
     } else {
         return vec![-5,-5,-5,-5];
     }
